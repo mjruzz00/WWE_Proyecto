286fb2fada205cca7fd5cd699f798317de47ebf6
mjruzz00 <99859690+mjruzz00@users.noreply.github.com>
2023-06-02 22:04:45 -0600

    Update Campeonato.cpp

--- Campeonato.cpp
+++ Campeonato.cpp
@@ -1,7 +1,15 @@
 #include "Campeonato.h"
 
 Campeonato::Campeonato(const std::string& nombre, int edad, const std::string& categoria)
-    : Luchador(nombre, edad), categoria(categoria) {}
+    : nombre(nombre), edad(edad), categoria(categoria) {}
+
+std::string Campeonato::obtenerNombre() const {
+    return nombre;
+}
+
+int Campeonato::obtenerEdad() const {
+    return edad;
+}
 
 std::string Campeonato::obtenerCategoria() const {
     return categoria;
