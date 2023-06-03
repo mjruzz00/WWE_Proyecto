4d99c29d5d2f66c316b47efd72fe6aec2b17d663
mjruzz00 <99859690+mjruzz00@users.noreply.github.com>
2023-06-02 22:01:50 -0600

    Update Arena.h

--- Arena.h
+++ Arena.h
@@ -3,15 +3,21 @@
 
 #include <string>
 
-class Arena {
+class Lugar {
+public:
+    virtual std::string obtenerNombre() const = 0;
+    virtual std::string obtenerUbicacion() const = 0;
+};
+
+class Arena : public Lugar {
 private:
     std::string nombre;
     std::string ubicacion;
 
 public:
     Arena(const std::string& nombre, const std::string& ubicacion);
-    std::string obtenerNombre() const;
-    std::string obtenerUbicacion() const;
+    std::string obtenerNombre() const override;
+    std::string obtenerUbicacion() const override;
 };
 
 #endif
