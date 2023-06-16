88d03860fd90a11a6f3ddb561d5ae438d6c05f5b
mjruzz00 <99859690+mjruzz00@users.noreply.github.com>
2023-06-16 07:24:47 -0600

    Update Arena.h

--- Arena.h
+++ Arena.h
@@ -1,23 +1,13 @@
 #ifndef ARENA_H
 #define ARENA_H
 
-#include <string>
-
-class Lugar {
-public:
-    virtual std::string obtenerNombre() const = 0;
-    virtual std::string obtenerUbicacion() const = 0;
-};
+#include "Lugar.h"
 
 class Arena : public Lugar {
-private:
-    std::string nombre;
-    std::string ubicacion;
-
 public:
     Arena(const std::string& nombre, const std::string& ubicacion);
-    std::string obtenerNombre() const override;
-    std::string obtenerUbicacion() const override;
+
+    void imprimirInformacion() const override;
 };
 
 #endif
